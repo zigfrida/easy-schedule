09d8b0f111944ced8a2cba9584a6a3a9b101bd42
Niko Arellano <narellano@my.bcit.ca>
2022-09-29 22:30:20 -0700

    feat(firebase): Add createFirebaseDao util + unit tests

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,19 +1,18 @@
-/* eslint-disable quotes */
 import  { initializeApp} from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyCbrO1ECZ70umezyA797kvvrXHSvVMtcPY",
-  authDomain: "easy-schedule-34f43.firebaseapp.com",
-  projectId: "easy-schedule-34f43",
-  storageBucket: "easy-schedule-34f43.appspot.com",
-  messagingSenderId: "1070031815112",
-  appId: "1:1070031815112:web:b27f7be81affaed6d040fd",
-  measurementId: "G-0KBKN0ZKYV"
+  apiKey: 'AIzaSyCbrO1ECZ70umezyA797kvvrXHSvVMtcPY',
+  authDomain: 'easy-schedule-34f43.firebaseapp.com',
+  projectId: 'easy-schedule-34f43',
+  storageBucket: 'easy-schedule-34f43.appspot.com',
+  messagingSenderId: '1070031815112',
+  appId: '1:1070031815112:web:b27f7be81affaed6d040fd',
+  measurementId: 'G-0KBKN0ZKYV'
 };
 
 const app = initializeApp(config)
 
 export const auth = getAuth(app)
-export const firestore = getFirestore(app);
+export const db = getFirestore(app);
