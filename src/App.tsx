7482b12e928750d5a76f1420aae3c602d02b29a0
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-10-05 22:00:15 -0700

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h1>Easy Schedule - Nurse Calendar App</h1>
+				<h1>Easy Schedule - Nurse Calendar App Kamal</h1>
 			</header>
 		</div>
 	);
