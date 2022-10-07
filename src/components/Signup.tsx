432452b1e61d0500c44c6fbd7a8b9dbf28de30fd
Amanda Golubics <amanda.golubics@gmail.com>
2022-10-06 20:24:04 -0700

    Fix UserType and signup form.

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -56,7 +56,7 @@ function Signup() {
             <Typography component='h1' variant='h5' sx={{ mt: '6rem' }}>
                 Sign up
             </Typography>
-            <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Box component='form' onSubmit={handleSubmit} sx={{ mt: 3 }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6}>
                         <TextField
