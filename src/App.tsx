a88a6283e59d1b6e82f672b996061e9114f8a3a4
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-10-10 22:31:32 -0700

    unit test-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signup from './components/Signup';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/signup' element={<Signup />} />
-            </Routes>
-        </BrowserRouter>
+        <div className='App'>
+            <header className='App-header'>
+                <h1>Easy Schedule - Nurse Calendar App</h1>
+            </header>
+        </div>
     );
 }
 
