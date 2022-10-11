19b8548caa5df028b93f228e2b0e3c8fc676b1ee
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-10-10 22:33:36 -0700

    failed unit test

--- src/api/__tests__/auth.spec.ts
+++ src/api/__tests__/auth.spec.ts
@@ -49,6 +49,9 @@ describe('createUserWithEmailAndPassword', () => {
     beforeEach(() => {
         jest.restoreAllMocks();
     });
+    test('Unit test failure', () => {
+        expect(1).toBe(2);
+    });
 
     test('should successfully create user', () => {
         createUserWithEmailAndPassword('new@new.com', 'test test');
