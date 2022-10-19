90fa4b302a569c05d01b67160b78ebf4d4385829
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-10-18 22:43:50 -0700

    revised

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,4 +1,9 @@
-import { AppBar, Box, Toolbar, Button, createTheme, ThemeProvider } from '@mui/material/';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/';
+import { ThemeProvider } from '@mui/material/';
 import { useLocation, Link } from 'react-router-dom';
 
 const theme = createTheme();
