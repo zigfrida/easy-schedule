2a6d1f9091ae31c82c021108ca55eb1234e82ddb
Niko Arellano <narellano@my.bcit.ca>
2022-10-30 18:59:56 -0700

    fix(appointments): Fix crash when opening appointments form

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -26,8 +27,8 @@ function AppointmentForm({ handleClose }: Props) {
     const [location, setLocation] = useState('');
     const [date, setDate] = useState<Dayjs | null>(dayjs());
 
-    const [nursesList] = useNursesList();
-    const defaultNurse = nursesList[0].uid;
+    const [nursesList, loading] = useNursesList();
+    const defaultNurse = nursesList[0]?.uid;
 
     const dateHandler = (newValue: Dayjs | null) => {
         setDate(newValue);
@@ -59,63 +60,77 @@ function AppointmentForm({ handleClose }: Props) {
             <Typography component='h1' variant='h5'>
                 Schedule New Appointment
             </Typography>
-            <Box component='form' onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <InputLabel id='demo-simple-select-label'>Nurse</InputLabel>
-                        <Select
-                            required
-                            labelId='demo-simple-select-label'
-                            id='demo-simple-select'
-                            value={nurse || defaultNurse}
-                            label='Nurse'
-                            onChange={nurseHandler}
-                        >
-                            {nursesList?.map((item) => (
-                                <MenuItem key={item.uid} value={item.uid}>
-                                    {item.firstName} {item.lastName}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            fullWidth
-                            id='title'
-                            label='Title'
-                            name='title'
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            fullWidth
-                            name='location'
-                            label='Location'
-                            id='location'
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DateTimePicker
-                                label='Date&Time picker'
-                                value={date}
-                                onChange={dateHandler}
-                                // eslint-disable-next-line react/jsx-props-no-spreading
-                                renderInput={(params) => <TextField {...params} />}
+            {loading ? (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        height: '100%',
+                        width: '100%',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <Box component='form' onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <InputLabel id='demo-simple-select-label'>Nurse</InputLabel>
+                            <Select
+                                required
+                                labelId='demo-simple-select-label'
+                                id='demo-simple-select'
+                                value={nurse || defaultNurse}
+                                label='Nurse'
+                                onChange={nurseHandler}
+                            >
+                                {nursesList?.map((item) => (
+                                    <MenuItem key={item.uid} value={item.uid}>
+                                        {item.firstName} {item.lastName}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id='title'
+                                label='Title'
+                                name='title'
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
                             />
-                        </LocalizationProvider>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name='location'
+                                label='Location'
+                                id='location'
+                                value={location}
+                                onChange={(e) => setLocation(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DateTimePicker
+                                    label='Date&Time picker'
+                                    value={date}
+                                    onChange={dateHandler}
+                                    // eslint-disable-next-line react/jsx-props-no-spreading
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-                    Schedule
-                </Button>
-            </Box>
+                    <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+                        Schedule
+                    </Button>
+                </Box>
+            )}
         </Container>
     );
 }
