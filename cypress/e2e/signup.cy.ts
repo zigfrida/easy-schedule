5400d17ef433ebfc079a4b7e51aa2b69430cc571
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-11-01 21:57:30 -0700

    ui test sign up

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -7,9 +7,9 @@ describe('signup', () => {
 
     it('should successfully sign up', () => {
         cy.contains('Sign up').should('exist');
-        cy.get('input#firstName').should('exist').type('Rob');
-        cy.get('input#lastName').should('exist').type('Star');
-        cy.get('input#email').should('exist').type('rob@gmail.com');
+        cy.get('input#firstName').should('exist').type('Olivia');
+        cy.get('input#lastName').should('exist').type('Bright');
+        cy.get('input#email').should('exist').type('olivia@gmail.com');
         cy.get('input#password').should('exist').type(COMMON_PASSWORD);
         cy.get('input#address').should('exist').type('8988 41 ave, Burnaby');
         cy.contains('Register').should('exist').click();
