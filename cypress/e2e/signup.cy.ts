8d221733e8e34b1261a28607d1d66b892114fb4c
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-11-02 10:37:06 -0700

    additions to sign up test

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -7,14 +7,20 @@ describe('signup', () => {
 
     it('should successfully sign up', () => {
         cy.contains('Sign up').should('exist');
-        cy.get('input#firstName').should('exist').type('Olivia');
-        cy.get('input#lastName').should('exist').type('Bright');
-        cy.get('input#email').should('exist').type('olivia@gmail.com');
+        cy.get('input#firstName').should('exist').type('lily');
+        cy.get('input#lastName').should('exist').type('Aniston');
+        cy.get('input#email').should('exist').type('lily@gmail.com');
         cy.get('input#password').should('exist').type(COMMON_PASSWORD);
         cy.get('input#address').should('exist').type('8988 41 ave, Burnaby');
         cy.contains('Register').should('exist').click();
 
         // Should be on Sign In page
         cy.contains('Sign In').should('exist');
+        cy.get('input#email').should('exist').type('lily@gmail.com');
+        cy.get('input#password').should('exist').type(COMMON_PASSWORD);
+        cy.contains('Submit').should('exist').click();
+
+        // Should be inside the app
+        cy.contains('Appointments').should('exist');
     });
 });
