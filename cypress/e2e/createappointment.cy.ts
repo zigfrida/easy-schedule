e8b9369261a3d3b304d36b5b9112e265006e3144
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-11-09 18:15:38 -0800

    merge new appointment and view details

--- cypress/e2e/createappointment.cy.ts
+++ cypress/e2e/createappointment.cy.ts
@@ -21,17 +21,25 @@ describe('Create New Appointment', () => {
         cy.get('input#location').should('exist').type('1123 star ave, Richmond');
         cy.get('button.MuiButtonBase-root:nth-child(1) > svg:nth-child(1)')
             .click()
-            .get('div.MuiDayPicker-weekContainer:nth-child(1) > button:nth-child(6)')
+            .get('div.MuiDayPicker-weekContainer:nth-child(2) > button:nth-child(6)')
             .click()
             .get('div.MuiPaper-root')
             .click()
             .get('.MuiClock-squareMask')
             .click();
+        // cy.wait(2000);
         cy.contains('button', 'Schedule').should('exist').click();
         cy.contains('Appointments').should('exist');
+        cy.wait(2000);
         // Check if the appointment details are displayed on the list
         cy.contains('help with food prep').should('exist');
         cy.contains('1123 star ave, Richmond').should('exist');
+    });
+    it('successfully view appointment details', () => {
+        cy.get('div.MuiBox-root:nth-child(2) > div:nth-child(1)').click();
+        cy.wait(1000);
+        cy.contains('Home').should('exist').click();
+        cy.wait(1000);
     });
     it('should successfully log out the user', () => {
         cy.contains('Logout').should('exist').click();
