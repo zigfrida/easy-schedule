5be1c53a332f6a6c6e05316321c3941f0a564c1d
Niko Arellano <narellano@my.bcit.ca>
2022-11-12 00:11:34 -0800

    fix(e2e): Fix sign up test to always use a new email

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,5 +1,7 @@
 import { COMMON_PASSWORD } from '../fixtures/credentials';
 
+const TEST_USER = `user-${Date.now()}@gmail.com`;
+
 describe('signup', () => {
     it('should successfully visit app', () => {
         cy.visit('https://easy-schedule-34f43.web.app/signup');
@@ -9,7 +11,7 @@ describe('signup', () => {
         cy.contains('Sign up').should('exist');
         cy.get('input#firstName').should('exist').type('Kamal');
         cy.get('input#lastName').should('exist').type('Sekhon');
-        cy.get('input#email').should('exist').type('kks@gmail.com');
+        cy.get('input#email').should('exist').type(TEST_USER);
         cy.get('input#password').should('exist').type(COMMON_PASSWORD);
         cy.get('input#address').should('exist').type('1001  98 ave, Vancouver');
         cy.contains('Register').should('exist').click();
@@ -18,7 +20,7 @@ describe('signup', () => {
         cy.contains('Sign In').should('exist');
     });
     it('new user should successfully signin', () => {
-        cy.get('input#email').should('exist').type('kks@gmail.com');
+        cy.get('input#email').should('exist').type(TEST_USER);
         cy.get('input#password').should('exist').type(COMMON_PASSWORD);
         cy.contains('Submit').should('exist').click();
 
